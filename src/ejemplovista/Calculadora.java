8ae286cfbf1484a66b3a8590b4fbc0b20966c60a
fey1975 <fabian.yapura@gmail.com>
2023-08-19 21:31:15 -0300

    Update Calculadora.java

--- src/ejemplovista/Calculadora.java
+++ src/ejemplovista/Calculadora.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejemplovista;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author diaser
- */
 public class Calculadora extends javax.swing.JFrame {
 
     /**
